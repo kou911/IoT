fe2423bcaae909565384fa6ae262df3d15054c77
kou911 <kotaf321@gmail.com>
2025-05-16 09:37:08 +0900

    5/16

--- flask/hello.py
+++ flask/hello.py
@@ -1,5 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+@app.route("/japan")
+def japan():
+    return "<p>Hello, 日本!</p>"
+
+@app.route("/neyagawa")
+def neyagawa():
+    return "<p>Hello, 寝屋川!</p>"
+
+@app.route("/kousen")
+def kousen():
+    return "<p>Hello, 高専!</p>"
